a6cf9d405fbedc854c1b284889598ae1ae683470
bcpratt2000@gmail.com <bcpratt2000@gmail.com>
2018-02-09 19:04:01 -0700

    updated jar

--- src/controller/Controller.java
+++ src/controller/Controller.java
@@ -26,13 +26,13 @@ public class Controller {
 
 		long startTime;
 		
-		Webcam.setDriver(new IpCamDriver());
-		try {
-			IpCamDeviceRegistry.register("Camera", "http://10.224.41.80:8080/video", IpCamMode.PULL);
-		} catch (MalformedURLException e1) {
-			// TODO Auto-generated catch block
-			e1.printStackTrace();
-		}
+//		Webcam.setDriver(new IpCamDriver());
+//		try {
+//			IpCamDeviceRegistry.register("Camera", "http://10.224.41.80:8080/video", IpCamMode.PULL);
+//		} catch (MalformedURLException e1) {
+//			// TODO Auto-generated catch block
+//			e1.printStackTrace();
+//		}
 		
 		webcam = Webcam.getDefault();
 		Dimension d = new Dimension(640, 480);
