6584d74603e2666629b0f816c8751ad243ad4ce0
bcpratt2000@gmail.com <bcpratt2000@gmail.com>
2018-02-12 09:10:44 -0700

    made Camera reconnect if disconnected

--- src/model/util/Camera.java
+++ src/model/util/Camera.java
@@ -13,30 +13,45 @@ import java.awt.image.ColorConvertOp;
 public class Camera {
 	Webcam webcam;
 	boolean isIpCamera;
+	BufferedImage image;
+	String url;
+
 	public Camera(int width, int height) {
+		this.url = null;
+		image = new BufferedImage(1, 1, 1);
 		Dimension d = new Dimension(width, height);
 		webcam = Webcam.getDefault();
 		webcam.setViewSize(d);
 		isIpCamera = false;
 		webcam.open();
 	}
+
 	public Camera(String url) throws MalformedURLException {
+		this.url = url;
+		image = new BufferedImage(1, 1, 1);
 		IpCamDeviceRegistry.register("Camera", url, IpCamMode.PUSH);
 		Webcam.setDriver(new IpCamDriver());
 		webcam = Webcam.getDefault();
 		isIpCamera = true;
 		webcam.open();
 	}
-	
+
 	public BufferedImage getImage() {
-		return webcam.getImage();
+		try {
+			image = webcam.getImage();
+		} catch (Exception e) {
+			try {
+				IpCamDeviceRegistry.register("Camera", url, IpCamMode.PUSH);
+			} catch (MalformedURLException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+			image = webcam.getImage();
+		}
+		return image;
 	}
+
 	public boolean isIpCamera() {
 		return isIpCamera;
 	}
 }
-
-
-
-
-
